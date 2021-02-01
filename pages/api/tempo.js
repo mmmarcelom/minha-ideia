0e216490f60893ebb0287b3749e03cd853fc7593
mmmarcelom <52570539+mmmarcelom@users.noreply.github.com>
2021-02-01 20:52:05 -0300

    Usando variável de ambiente

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,17 @@
-function tempo(request, response){
+async function tempo(request, response){
     const dynamicDate = new Date();
+    const apiSecret = process.env.CONVERTKIT_API_SECRET
+
+    const estadosResponse = await fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados/SP/municipios")
+    const estadosResponseJson = await estadosResponse.json()
+    const quantidade = estadosResponseJson.length
+    
 
     response.json({
-        date: dynamicDate.toTimeString()
+        date: dynamicDate.toTimeString(),
+        quantidade: quantidade,
+        teste: apiSecret
+        
     });
 }
 
